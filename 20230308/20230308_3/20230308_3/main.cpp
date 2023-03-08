06cf768e9a386fc6bc8f6e9895c693367c0736ad
김승준 <125628940+skdldk@users.noreply.github.com>
2023-03-08 10:21:23 +0900

    Add files via upload

---
+++ 20230308/20230308_3/20230308_3/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("1x1 =1\n");
+	printf("1x2 =2\n");
+	printf("1x3 =3\n");
+	printf("1x4 =4\n");
+	printf("1x5 =5\n");
+	printf("1x6 =6\n");
+	printf("1x7 =7\n");
+	printf("1x8 =8\n");
+	printf("1x9 =9\n");
+
+	return (0);
+}
