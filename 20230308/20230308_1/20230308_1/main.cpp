fd2fa8aa415ce5424e6be27fa3f4239630335139
김승준 <125628940+skdldk@users.noreply.github.com>
2023-03-08 09:51:24 +0900

    Add files via upload

---
+++ 20230308/20230308_1/20230308_1/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>   //hader 파일추가 strandard 10
+
+
+int main(void)
+{
+
+	printf("test\n");  // \n line feed
+	printf("202315003\n");  // \n line feed
+	printf("KIM Seung Jun\n");  // \n line feed
+
+
+	return 0;
+}
+
+// ctrl+F5 는 실행
