d35b0a68433d244ff9bfb3c4db16a46efdc6a306
김승준 <125628940+skdldk@users.noreply.github.com>
2023-04-12 01:07:50 +0900

    Add files via upload

---
+++ c언어 도전 문제/20230410/20230410_1/20230410_1/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int c = 0;
+
+	c = a + b;
+
+	printf("%d + %d + %d", a, b, c);
+
+	return 0;
+
+}
---
+++ c언어 도전 문제/20230410/20230410_2/20230410_2/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 1.0;
+	float PI = 3.141592;
+	float z = 4.141592;
+
+	printf("%d + %f = %f", x, PI, z);
+
+	return 0;
+
+
+}
